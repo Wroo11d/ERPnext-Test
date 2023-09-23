f903b900a13a5d57404ce71a48a6506e1627883c
worood esam <worood.it@gmail.com>
2023-09-23 08:48:39 +0300

    First Commit

---
+++ business_theme_v14/public/js/test5.js
@@ -0,0 +1,44 @@
+frappe.provide('frappe.ui.toolbar');
+
+frappe.ui.toolbar.add_navbar_item = function() {
+    alert('Function add_navbar_item is called'); // Alert when function is called
+    console.log("--------------------------\n-------------------------");
+    // Target the navbar section
+    
+    //document.getElementById("navbar-breadcrumbs").append("<li>heyyyyyyyyyyyyyyyyyyyyyy</li>");
+
+    const navbar = document.querySelector(".collapse.navbar-collapse.justify-content-end"); //${".classname}
+    if (!navbar) {
+        alert('Navbar not found'); // Alert if navbar is not found
+        return;
+    }
+
+    alert('Navbar found'); // Alert if navbar is found
+    
+    
+    // Create new navbar items
+    const newItem = `
+    <ul class="navbar-nav">
+        <li class="cnav-item">
+            <a href="http://www.erpnext.com">New Item 1</a>
+        </li>
+        <li class="cnav-item" >
+            <a href="http://www.erpnext.com">New Item 2</a>
+        </li>
+        <li class="cnav-item">
+            <a href="http://www.erpnext.com">New Item 3</a>
+        </li>
+    </ul>
+    `;
+    
+    // Add the new items right after the logo
+    navbar.insertAdjacentHTML('afterbegin', newItem);
+    alert('Navbar items added'); // Alert after adding navbar items
+    
+}
+
+// Wait for the DOM content to be fully loaded before calling the function
+document.addEventListener('DOMContentLoaded', (event) => {
+    // You can adjust the delay as needed
+    setTimeout(frappe.ui.toolbar.add_navbar_item, 2000); // 2000 milliseconds = 2 seconds
+})
---
+++ business_theme_v14/public/js/ttest9.js
@@ -0,0 +1,87 @@
+frappe.provide('frappe.ui.toolbar');
+
+frappe.ui.toolbar.add_navbar_item = function () {
+    const content = [
+        {
+            name:"Customers",
+            link:"http://127.0.0.1:8000/app/customer"
+        },
+        {
+            name:"Subscription",
+            link:"http://127.0.0.1:8000/app/subscription"
+        },
+        {
+            name:"Sales Invoice",
+            link:"http://127.0.0.1:8000/app/sales-invoice"
+        }
+    ];
+    
+    // Target the sidebar section
+
+    const sidebar = document.querySelector('[data-title="Public"]'); //${".classname}
+    if (!sidebar) {
+        alert('sidebar not found'); // Alert if sidebar is not found
+        return;
+    }
+
+    function createItem(name, link) {
+        return (`
+        <div class="sidebar-item-container is-draggable" item-parent="" item-name=${name} item-public="1" item-is-hidden="0">
+                     <div class="desk-sidebar-item standard-sidebar-item">
+                         <a href=${link} class="item-anchor" title=${name}>
+                             <span class="sidebar-item-icon" item-icon="getting-started"><svg class="icon  icon-md" style="">
+                 <use class="" href="#icon-getting-started"></use>
+             </svg></span>
+                             <span class="sidebar-item-label">${name}<span>
+                         </span></span></a>
+                         <div class="sidebar-item-control"><button class="btn btn-secondary btn-xs drag-handle" title="Drag"><svg class="icon  icon-xs" style="">
+                 <use class="" href="#icon-drag"></use>
+             </svg></button><div class="btn btn-secondary btn-xs setting-btn dropdown-btn" title="Setting">
+                     <svg class="icon  icon-xs" style="">
+                 <use class="" href="#icon-dot-horizontal"></use>
+             </svg>
+                 </div>
+                 <div class="dropdown-list hidden"><div class="dropdown-item" title="Edit Workspace">
+                         <span class="dropdown-item-icon"><svg class="icon  icon-sm" style="">
+                 <use class="" href="#icon-edit"></use>
+             </svg></span>
+                         <span class="dropdown-item-label">Edit</span>
+                     </div><div class="dropdown-item" title="Duplicate Workspace">
+                         <span class="dropdown-item-icon"><svg class="icon  icon-sm" style="">
+                 <use class="" href="#icon-duplicate"></use>
+             </svg></span>
+                         <span class="dropdown-item-label">Duplicate</span>
+                     </div><div class="dropdown-item" title="Hide Workspace">
+                         <span class="dropdown-item-icon"><svg class="icon  icon-sm" style="">
+                 <use class="" href="#icon-hide"></use>
+             </svg></span>
+                         <span class="dropdown-item-label">Hide</span>
+                     </div><div class="dropdown-item" title="Delete Workspace">
+                         <span class="dropdown-item-icon"><svg class="icon  icon-sm" style="">
+                 <use class="" href="#icon-delete-active"></use>
+             </svg></span>
+                         <span class="dropdown-item-label">Delete</span>
+                     </div></div><span class="drop-icon hidden"><svg class="icon  icon-sm" style="">
+                 <use class="" href="#icon-small-down"></use>
+             </svg></span></div>
+                     </div>
+                     <div class="sidebar-child-item nested-container"></div>
+                 </div>
+         `)
+    }
+
+    content.forEach(element => {
+        // Create new sidebar item
+        const newItem = createItem(element.name, element.link);
+        // Add the new item at the end
+        sidebar.insertAdjacentHTML('beforeend', newItem);
+    });
+
+}
+
+// Wait for the DOM content to be fully loaded before calling the function
+document.addEventListener('DOMContentLoaded', (event) => {
+    // You can adjust the delay as needed
+    setTimeout(frappe.ui.toolbar.add_navbar_item, 2000); // 2000 milliseconds = 2 seconds
+})
+
